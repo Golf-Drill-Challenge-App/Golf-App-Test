8c3cff923460cad5874854b5798d93b249f0ae97
Frankreed <baoanhtdn@gmail.com>
2023-12-22 15:06:34 -0800

    tamagui test. selector works

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button, TamaguiProvider} from 'tamagui'
+
+import config from './tamagui.config'
+import {SelectDemoItem} from "./Select";
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [val, setVal] = useState('apple')
+    return (
+        <TamaguiProvider config={config}>
+            <View style={styles.container}>
+                <Button>Hello world</Button>
+                <SelectDemoItem items={[
+                    {name: 'Apple'},
+                    {name: 'Pear'},
+                    {name: 'Blackberry'},
+                    {name: 'Peach'},
+                    {name: 'Apricot'},
+                    {name: 'Melon'},
+                    {name: 'Honeydew'},
+                    {name: 'Starfruit'},
+                    {name: 'Blueberry'},
+                    {name: 'Raspberry'},
+                    {name: 'Strawberry'},
+                    {name: 'Mango'},
+                    {name: 'Pineapple'},
+                    {name: 'Lime'},
+                    {name: 'Lemon'},
+                    {name: 'Coconut'},
+                    {name: 'Guava'},
+                    {name: 'Papaya'},
+                    {name: 'Orange'},
+                    {name: 'Grape'},
+                    {name: 'Jackfruit'},
+                    {name: 'Durian'}
+                ]} label={"Fruits"} val={val} setVal={setVal} native={true}/>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto"/>
+                <Text>Selected: {val}</Text>
+            </View>
+        </TamaguiProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
