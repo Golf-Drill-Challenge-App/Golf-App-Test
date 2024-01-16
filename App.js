3af459d568ddb878bd930a56d3d19bbce9fef976
Jake Gehrke <gehrkej@oregonstate.edu>
2024-01-15 19:45:35 -0800

    set up to display DrillSubmission Test Screen

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
+import DrillSubmission from "./screens/DrillSubmission";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <SafeAreaView>
+        <DrillSubmission />
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
