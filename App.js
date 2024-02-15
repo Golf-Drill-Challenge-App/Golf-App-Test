aafd5354a56ff0ba12d0a11bcd9b334c365c008d
Jake Gehrke <gehrkej@oregonstate.edu>
2024-02-14 19:47:47 -0800

    got fonts to work with react native base text component

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "IBMPlexMono-Regular": require("./assets/fonts/IBMPlexMono-Regular.ttf"),
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+    "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: "IBMPlexMono-Regular", fontSize: 30 }}>
+        IBMPlexMono Regular
+      </Text>
+      <Text style={{ fontFamily: "Inter-Bold", fontSize: 30 }}>Inter-Bold</Text>
+      <Text style={{ fontFamily: "Inter-Regular", fontSize: 30 }}>
+        Inter-Regular
+      </Text>
+      <Text style={{ fontFamily: "Inter-SemiBold", fontSize: 30 }}>
+        Inter-SemiBold
+      </Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
     </View>
   );
 }
@@ -13,8 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
